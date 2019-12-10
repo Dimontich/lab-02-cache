c0b654c5d338ea055657e16971c2c6306caee17a
dmitrij <dimasek28@gmail.com>
2019-12-10 08:58:59 +0300

    work

--- sources/Investigation.cpp
+++ sources/Investigation.cpp
@@ -1,74 +1,67 @@
 // Copyright 2019 Dimontich
 #include "Investigation.h"
 
-
-Investigation::Investigation()
-{
-    buffer.first_size = 0.5 * cache.min_size;
-    buffer.current_size = buffer.first_size;
-    buffer.last_size = 1.5 * cache.max_size;
+Investigation::Investigation() {
+  buffer.first_size = 0.5 * cache.min_size;
+  buffer.current_size = buffer.first_size;
+  buffer.last_size = 1.5 * cache.max_size;
 }
 
-void Investigation::Begin_expirement()
-{
-    uint64_t elements_amount = buffer.first_size / sizeof(uint64_t);
-    uint64_t cache_size_degree = log2(buffer.first_size);
-    while (buffer.current_size <= buffer.last_size)
-    {
-        auto *experiment = new Experiment(elements_amount);
-        experiment->Begin_passages();
-        result.push_back(experiment);
-        cache_size_degree++;
-        buffer.current_size = pow(2, cache_size_degree);
-        elements_amount = buffer.current_size / sizeof(uint64_t);
-    }
+void Investigation::Begin_expirement() {
+  uint64_t elements_amount = buffer.first_size / sizeof(uint64_t);
+  uint64_t cache_size_degree = log2(buffer.first_size);
+  while (buffer.current_size <= buffer.last_size) {
+    auto* experiment = new Experiment(elements_amount);
+    experiment->Begin_passages();
+    result.push_back(experiment);
+    cache_size_degree++;
+    buffer.current_size = pow(2, cache_size_degree);
+    elements_amount = buffer.current_size / sizeof(uint64_t);
+  }
 }
 
-void Investigation::End_expirement(std::ofstream& file)
-{
-    file << "investigaion:" << std::endl;
-    file << " travel_order: \"Direction\"" << std::endl;
-    file << " experiments:" << std::endl;
-    file << " - experiment:" << std::endl;
-    for (size_t i = 0; i < result.size(); i++)
-    {
-        file << "  number: " << i + 1 << std::endl;
-        file << "  input_data:" << std::endl;
-        file << "   buffer_size:" << result[i]->elements_amount *
-        sizeof(uint64_t) / 1024<< " kB" << std::endl;
-        file << "  results:" << std::endl;
-        file << "   duration:" << result[i]->time.direct << " ms" << std::endl;
-    }
+void Investigation::End_expirement(std::ofstream& file) {
+  file << "investigaion:" << std::endl;
+  file << " travel_order: \"Direction\"" << std::endl;
+  file << " experiments:" << std::endl;
+  file << " - experiment:" << std::endl;
+  for (size_t i = 0; i < result.size(); i++) {
+    file << "  number: " << i + 1 << std::endl;
+    file << "  input_data:" << std::endl;
+    file << "   buffer_size:"
+         << result[i]->elements_amount * sizeof(uint64_t) / 1024 << " kB"
+         << std::endl;
+    file << "  results:" << std::endl;
+    file << "   duration:" << result[i]->time.direct << " ms" << std::endl;
+  }
 
-    file << "investigaion:" << std::endl;
-    file << " travel_order: \"Reverse\"" << std::endl;
-    file << " experiments:" << std::endl;
-    file << " - experiment:" << std::endl;
-    for (size_t i = 0; i < result.size(); i++)
-    {
-        file << "  number: " << i + 1 << std::endl;
-        file << "  input_data:" << std::endl;
-        file << "   buffer_size:" << result[i]->elements_amount *
-        sizeof(uint64_t) / 1024<< " kB" << std::endl;
-        file << "  results:" << std::endl;
-        file << "   duration:" << result[i]->time.reverse << " ms" << std::endl;
-    }
+  file << "investigaion:" << std::endl;
+  file << " travel_order: \"Reverse\"" << std::endl;
+  file << " experiments:" << std::endl;
+  file << " - experiment:" << std::endl;
+  for (size_t i = 0; i < result.size(); i++) {
+    file << "  number: " << i + 1 << std::endl;
+    file << "  input_data:" << std::endl;
+    file << "   buffer_size:"
+         << result[i]->elements_amount * sizeof(uint64_t) / 1024 << " kB"
+         << std::endl;
+    file << "  results:" << std::endl;
+    file << "   duration:" << result[i]->time.reverse << " ms" << std::endl;
+  }
 
-    file << "investigaion:" << std::endl;
-    file << " travel_order: \"Random\"" << std::endl;
-    file << " experiments:" << std::endl;
-    file << " - experiment:" << std::endl;
-    for (size_t i = 0; i < result.size(); i++)
-    {
-        file << "  number: " << i + 1 << std::endl;
-        file << "  input_data:" << std::endl;
-        file << "   buffer_size:" << result[i]->elements_amount *
-        sizeof(uint64_t) / 1024 << " kB" << std::endl;
-        file << "  results:" << std::endl;
-        file << "   duration:" << result[i]->time.random << " ms" << std::endl;
-    }
+  file << "investigaion:" << std::endl;
+  file << " travel_order: \"Random\"" << std::endl;
+  file << " experiments:" << std::endl;
+  file << " - experiment:" << std::endl;
+  for (size_t i = 0; i < result.size(); i++) {
+    file << "  number: " << i + 1 << std::endl;
+    file << "  input_data:" << std::endl;
+    file << "   buffer_size:"
+         << result[i]->elements_amount * sizeof(uint64_t) / 1024 << " kB"
+         << std::endl;
+    file << "  results:" << std::endl;
+    file << "   duration:" << result[i]->time.random << " ms" << std::endl;
+  }
 }
 
-Investigation::~Investigation()
-= default;
-
+Investigation::~Investigation() = default;
--- sources/lab02_cache.cpp
+++ sources/lab02_cache.cpp
@@ -1,15 +1,12 @@
 // Copyright 2019 Dimontich
 #include "Investigation.h"
 
-int main()
-{
-    Investigation investigation;
-    investigation.Begin_expirement();
+int main() {
+  Investigation investigation;
+  investigation.Begin_expirement();
 
-    std::ofstream path_to_report("../reports/report.txt\n");
-    investigation.End_expirement(path_to_report);
+  std::ofstream path_to_report("../reports/report.txt\n");
+  investigation.End_expirement(path_to_report);
 
-    return 0;
+  return 0;
 }
-
-
