446e3adf8dfee88461fdda30292905b200cefdb4
dmitrij <dimasek28@gmail.com>
2019-12-10 09:14:51 +0300

    5

--- cmake-build-debug/CTestTestfile.cmake
+++ cmake-build-debug/CTestTestfile.cmake
@@ -4,5 +4,5 @@
 # 
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
-add_test(unit_tests "tests")
-set_tests_properties(unit_tests PROPERTIES  _BACKTRACE_TRIPLES "/Users/dmitrij/lab-02/CMakeLists.txt;26;add_test;/Users/dmitrij/lab-02/CMakeLists.txt;0;")
+add_test(unit_tests "/Users/dmitrij/lab-02/cmake-build-debug/tests")
+set_tests_properties(unit_tests PROPERTIES  _BACKTRACE_TRIPLES "/Users/dmitrij/lab-02/CMakeLists.txt;32;add_test;/Users/dmitrij/lab-02/CMakeLists.txt;0;")
